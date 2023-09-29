09178cd29ccfa7a2e622f3021cb0c1644733b087
Tke-Huncho <107799872+Tke-Huncho@users.noreply.github.com>
2023-09-29 05:27:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+# Hans Matere SCT 211-0080/2022
+
+num1 = int(input("Please enter number one: "))
+num2 = int(input("Please enter number two: "))
+
+sum = int(num1 + num2)
+
+print("The sum of the two numbers is: " +str(sum))
